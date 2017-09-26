572bbd38fbd9bad179d73cb1d0d54063c006c3a3
dmitriydemchuk <dmitry.demchuck@nextiva.com>
2017-09-26 16:47:14 +0300

    changed other classes for longer text

--- src/main/java/com/ucms/domain/Comment.java
+++ src/main/java/com/ucms/domain/Comment.java
@@ -1,60 +1,66 @@
 package com.ucms.domain;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
 
 @Entity
 public class Comment {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-	@NotNull
-	@Column(name = "value", nullable = false)
-	private String comment;
+    @NotNull
+    @Column(name = "value", nullable = false, length = 1000)
+    @Type(type = "org.hibernate.type.StringClobType")
+    private String comment;
 
-	@NotNull
-	@Column(name = "id_topic", nullable = false)
-	private Long topic;
+    @NotNull
+    @Column(name = "id_topic", nullable = false)
+    private Long topic;
 
-	@CreationTimestamp
-	@Column(name = "create_date")
-	private Date createDateTime;
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date createDateTime;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getComment() {
-		return comment;
-	}
+    public String getComment() {
+        return comment;
+    }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 
-	public Long getTopic() {
-		return topic;
-	}
+    public Long getTopic() {
+        return topic;
+    }
 
-	public void setTopic(Long topic) {
-		this.topic = topic;
-	}
+    public void setTopic(Long topic) {
+        this.topic = topic;
+    }
 
-	public Date getCreateDateTime() {
-		return createDateTime;
-	}
+    public Date getCreateDateTime() {
+        return createDateTime;
+    }
 
-	public void setCreateDateTime(Date createDateTime) {
-		this.createDateTime = createDateTime;
-	}
+    public void setCreateDateTime(Date createDateTime) {
+        this.createDateTime = createDateTime;
+    }
 }
